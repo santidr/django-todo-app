dcbbce353b7c4e771b1d618ad74fceb15b17ab8b
Santiago Diaz <kerbindr@gmail.com>
2020-03-29 21:31:00 -0400

    Bootstrap styling and new base.html template added

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,3 +6,15 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('title',)
+        labels = {
+            'title': False
+        }
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control mr-sm-2',
+                    'placeholder': 'Enter a new task',
+                    'style': 'width: 60%;',
+                }
+            )
+        }
